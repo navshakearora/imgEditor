81a86a12ceb23eb9d1c31952ca506752a6c39fb9
navshakearora <navshakearora@github.com>
2019-11-01 18:13:29 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,46 +1,62 @@
 (function() {
-	"use strict";
-console.log("SEAF has fired");	
+  "use strict";
+console.log("SEAF has fired");  
 
- var rightSide = document.queryselector("#rightSide");
- var chngImg = document.queryselector("#chngImg");
+ var button1 = document.querySelector ("#blur");
+
+ var button2 = document.querySelector ("#grayscale");
+
+ var button3 = document.querySelector ("#contrast");
+
+ var button4 = document.querySelector ("#brightness");
+
+ var button5 = document.querySelector ("#hue");
+
+ var button6 = document.querySelector ("#blackwhite");
+
+ var button7 = document.querySelector ("#rotate");
+
+ var button8 = document.querySelector ("#sepia");
+
+ var chngImg = document.querySelector ("#chngImg");
 
 
   
   function blur(){
-  	 img.style.webkitFilter = "blur(20px)";
-  	}
-  	 function grayscale(){
-  	 img.style.webkitFilter = "grayscale(60%)";
-  	}
-  	function contrast(){
-  	 img.style.webkitFilter = "contrast(60%)";
-  	}
- 	function brightness(){
-  	 img.style.webkitFilter = "brightness(0.4)";
-  	}
-  	function hue(){
-  	 img.style.webkitFilter = "hue-rotate(90deg)";
-  	}
-  	function blackwhite(){
-  	 img.style.webkitFilter = "grayscale(100%)";
-  	}
-  	function rotate(){
-  	 img.style.webkitFilter = "rotate(180deg)";
-  	}
-  	function sepia(){
-  	 img.style.webkitFilter = "sepia(100%)";
-  	}
+     chngImg.style.WebkitFilter = "blur(20px)";
+    }
+    function grayscale(){
+     chngImg.style.WebkitFilter = "grayscale(50%)";
+    }
+    function contrast(){
+     chngImg.style.WebkitFilter = "contrast(60%)";
+    }
+    function brightness(){
+     chngImg.style.WebkitFilter = "brightness(150%)";
+    }
+    function hue(){
+     chngImg.style.WebkitFilter = "hue-rotate(90deg)";
+    }
+    function blackwhite(){
+     chngImg.style.WebkitFilter = "grayscale(100%)";
+    }
+    function rotate(){
+     chngImg.style.WebkitTransform = "rotate(180deg)";
+    }
+    function sepia(){
+     chngImg.style.WebkitFilter = "sepia(100%)";
+     console.log("sepia called")
+    }
 
   //listeners
 
-  button.addEventListener("click", Blur, false);
-  button.addEventListener("click", greyscale , false);
-  button.addEventListener("click", contrast , false);
-  button.addEventListener("click", brightness , false);
-  button.addEventListener("click", hue , false);
-  button.addEventListener("click", blackwhite , false);
-  button.addEventListener("click", rotate , false);
-  button.addEventListener("click", sepia , false);
+  button1.addEventListener("click", blur, false);
+  button2.addEventListener("click", grayscale , false);
+  button3.addEventListener("click", contrast , false);
+  button4.addEventListener("click", brightness , false);
+  button5.addEventListener("click", hue , false);
+  button6.addEventListener("click", blackwhite , false);
+  button7.addEventListener("click", rotate , false);
+  button8.addEventListener("click", sepia , false);
 
 })();
